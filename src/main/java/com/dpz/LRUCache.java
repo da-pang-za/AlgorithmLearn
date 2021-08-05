5ddbf5d087d785a5251c314afd8f81641bede360
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2021-08-05 18:16:30 +0800

    LRU

--- src/main/java/com/dpz/LRUCache.java
+++ src/main/java/com/dpz/LRUCache.java
@@ -1,114 +1,74 @@
 package com.dpz;
 
 import java.util.HashMap;
+
 class LRUCache {
-
-    public static void main(String[] args) {
-        LRUCache lru=new LRUCache(2);
-        lru.put(1,1);
-        lru.put(2,2);
-        lru.get(1);
-        lru.put(3,3);
-        lru.get(2);
-
-        lru.put(4,4);
-        lru.get(1);
-        lru.get(3);
-        lru.get(4);
-
-
-
-    }
     static class Node{
         int key;
         int val;
-        Node pre;
         Node next;
-        Node(int k,int v){
+        Node pre;
+        Node(){}
+        Node(int k ,int v){
             key=k;
             val=v;
         }
     }
-    Node head;
-    Node tail;
     int capacity;
-    int cur=0;//当前容量
-    HashMap<Integer, Node>map;
+    int curSize;
+    Node head=new Node();
+    Node tail=new Node();
+    HashMap<Integer,Node> map=new HashMap<>();
     public LRUCache(int capacity) {
-        head=null;
-        tail=null;
         this.capacity=capacity;
-        map=new HashMap<>();
-
+        curSize=0;
+        head.next=tail;
+        tail.pre=head;
     }
 
     public int get(int key) {
-        if(map.containsKey(key)){
-            Node node= map.get(key);
-            //把此节点放到头部
-            if(node!=head){//注意
-                delete(node);
-                add2Head(node);
-            }
-            return node.val;
-
+        if(!map.containsKey(key)){
+            return -1;
         }
-        return -1;
-
+        return addTail(key).val;
     }
 
-    private void add2Head(Node node) {
-        if(head==null){
-            head=node;
-            tail=node;
-        }
-        else {
-            head.next=node;
-            node.pre=head;
-            head=node;
-        }
-
-    }
-    //已经保证存在此节点
-    private void delete(Node node) {
-        if(node==tail){
-            node=tail.next;
-            node.pre=null;
-            tail.next=null;
-            tail=node;
-        }
-        else {
-            node.pre.next=node.next;
-            node.next.pre=node.pre;
-            node.pre=null;
-            node.next=null;
-        }
-
-    }
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            Node node= map.get(key);
-            //把此节点放到头部
-            node.val=value;
-            if(node!=head){//注意
-                delete(node);
-                add2Head(node);
-            }
+        if(!map.containsKey(key)){
+
+            Node node=new Node(key,value);
+            //加到tail
+            node.next=tail;
+            node.pre=tail.pre;
+            tail.pre.next=node;
+            tail.pre=node;
+
+            map.put(key,node);
+            curSize++;
         }
         else{
-            //添加
-            Node node= new Node(key, value);
-            //加到链表和HashMap中
-            add2Head(node);
-            map.put(key,node);
-            if(cur==capacity){
-                map.remove(tail.key);
-                delete(tail);
-            }
-            else cur++;
+            addTail(key).val=value;
         }
-
+        if(curSize>capacity){
+            curSize--;
+            Node node=head.next;
+            head.next=node.next;
+            node.next.pre=head;
+            node.next=node.pre=null;
+            map.remove(node.key);
+        }
     }
-
-
+    //拆下来放到尾部
+    Node addTail(int key){
+        Node node=map.get(key);
+        node.pre.next=node.next;
+        node.next.pre=node.pre;
+        node.next=node.pre=null;
+        //加到tail
+        node.next=tail;
+        node.pre=tail.pre;
+        tail.pre.next=node;
+        tail.pre=node;
+        return node;
+    }
 }
