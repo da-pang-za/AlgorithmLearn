7c6c870f1d1226c9bad4c8b11226f670212c9a05
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-01-26 15:32:54 +0800

    增加嵌套构造函数  和  toString()方法

--- src/main/java/com/dpz/dataStructure/TreeNode.java
+++ src/main/java/com/dpz/dataStructure/TreeNode.java
@@ -1,13 +1,27 @@
 package com.dpz.dataStructure;
 
 
-
-public  class TreeNode {
+public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+
+    }
+
     public TreeNode(int x) {
         val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return ""+val;
     }
 }
