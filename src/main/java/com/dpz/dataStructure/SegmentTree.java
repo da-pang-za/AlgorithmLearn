537dc3dc51399fcaa54e969ae8e218c5d6106df4
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-01-26 15:32:18 +0800

    线段树

--- src/main/java/com/dpz/dataStructure/SegmentTree.java
+++ src/main/java/com/dpz/dataStructure/SegmentTree.java
@@ -1,6 +1,8 @@
 package com.dpz.dataStructure;
+//线段树讲解：https://zhuanlan.zhihu.com/p/106118909
 
-/** todo
+/**
+ * todo 动态开点  lazy标记  数组实现
  * 线段树
  * 线段树的用处就是，对编号连续的一些点进行修改或者统计操作，修改和统计的复杂度都是O(log2(n)).
  * 线段树中，每个点代表一条线段（区间）
@@ -14,129 +16,90 @@ package com.dpz.dataStructure;
  * 众数——只知道左右区间的众数，没法求总区间的众数
  */
 
+class SegmentTree {
+    int sum;
+    int left;
+    int right;
+    SegmentTree lNode;
+    SegmentTree rNode;
 
-public abstract class SegmentTree {
+    SegmentTree(int l, int r, int[] nums) {
+        left = l;
+        right = r;
 
-    int maxn;//元素总个数 / 区间长度
-    int[] Sum, Add;//Sum求和，Add为懒惰标记
-    int[] A;//存原数组数据下标[1,n]
-    int n;
-
-    SegmentTree(int maxn) {
-        this.maxn = maxn;
-        Sum = new int[maxn << 2];
-        Add = new int[maxn << 2];
-        A = new int[maxn];
-        Build(1, maxn - 1, 1);
+        if (l != r) {
+            int mid = left + right >> 1;
+            lNode = new SegmentTree(l, mid, nums);
+            rNode = new SegmentTree(mid + 1, r, nums);
+            sum = lNode.sum + rNode.sum;
+        } else sum = nums[l];
     }
 
-    //PushUp函数更新节点信息 ，这里是求和
+    void add(int l, int r, int val) {
+        if (l > right || r < left) return;
+        if (left == right) sum += val;
+        else {
+            lNode.add(l, r, val);
+            rNode.add(l, r, val);
+            sum = lNode.sum + rNode.sum;
+        }
+    }
 
+    int query(int l, int r) {
+        if (l > right || r < left) return 0;
+        if (l <= left && right <= r) return sum;
+        return lNode.query(l, r) + rNode.query(l, r);
+    }
 
-    /**
-     * Build 建树
-     */
-    void Build(int l, int r, int rt) { //l,r表示当前节点区间，rt表示当前节点编号
-        if (l == r) {//若到达叶节点
-            Sum[rt] = A[l];//储存数组值
+}
+
+class SegmentTreeArr {
+    int MAXN = (int) 1e5 + 5;
+    int[] tree = new int[MAXN << 2], mark = new int[MAXN << 2], A = new int[MAXN];
+    int n, m;
+
+    void push_down(int p, int len) {
+        if (len <= 1) return;
+        tree[p << 1] += mark[p] * (len - len / 2);
+        mark[p << 1] += mark[p];
+        tree[p << 1 | 1] += mark[p] * (len / 2);
+        mark[p << 1 | 1] += mark[p];
+        mark[p] = 0;
+    }
+
+    void build(int p, int cl, int cr) {
+        if (cl == cr) {
+            tree[p] = A[cl];
             return;
         }
-        int m = (l + r) >> 1;
-        //左右递归
-        Build(l, m, rt << 1);
-        Build(m + 1, r, rt << 1 | 1);
-        //更新信息
-        PushUp(rt);
+//      return void(tree[p] = A[cl]);
+        int mid = (cl + cr) >> 1;
+        build(p << 1, cl, mid);
+        build(p << 1 | 1, mid + 1, cr);
+        tree[p] = tree[p << 1] + tree[p << 1 | 1];
     }
 
-    /**
-     * 上推    根据子节点更新当前节点
-     *
-     * @param rt 父节点下标
-     */
-    void PushUp(int rt) {
-        Sum[rt] = Sum[rt << 1] + Sum[rt << 1 | 1];//左右相加
+    int query(int l, int r, int p, int cl, int cr) {
+        if (cl >= l && cr <= r) return tree[p];
+        push_down(p, cr - cl + 1);
+        int mid = (cl + cr) >> 1, ans = 0;
+        if (mid >= l) ans += query(l, r, p << 1, cl, mid);
+        if (mid < r) ans += query(l, r, p << 1 | 1, mid + 1, cr);
+        return ans;
     }
 
-    /**
-     * 点修改
-     * 假设A[L]+=C
-     */
-    void Update(int L, int C, int l, int r, int rt) {//l,r表示当前节点区间，rt表示当前节点编号
-        if (l == r) {//到叶节点，修改
-            Sum[rt] += C;
-            return;
+    void update(int l, int r, int d, int p, int cl, int cr) {
+        if (cl >= l && cr <= r) {
+            tree[p] += d * (cr - cl + 1);
+            mark[p] += d;
         }
-        int m = (l + r) >> 1;
-        //先更新子节点
-        //根据条件判断往左子树调用还是往右
-        if (L <= m) Update(L, C, l, m, rt << 1);
-        else Update(L, C, m + 1, r, rt << 1 | 1);
-        PushUp(rt);//更新当前节点
-    }
-
-    /**
-     * 区间修改
-     * 假设A[L,R]+=C
-     */
-    void Update(int L, int R, int C, int l, int r, int rt) {//L,R表示操作区间，l,r表示当前节点区间，rt表示当前节点编号
-        //如果本区间完全在操作区间[L,R]以内
-        //即当前整个区间都需要修改
-        if (L <= l && r <= R) {
-            Sum[rt] += C * (r - l + 1);//更新数字和，向上保持正确
-            //打标记  下面的子区间不动
-            Add[rt] += C;//增加Add标记，表示本区间的Sum正确，子区间的Sum仍需要根据Add的值来调整
-            return;
-        }
-        int m = (l + r) >> 1;
-        PushDown(rt, l, r);//下推标记  这个是之前的标记
-        //这里判断左右子树跟[L,R]有无交集，有交集才递归
-        if (L <= m) Update(L, R, C, l, m, rt << 1);
-        if (R > m) Update(L, R, C, m + 1, r, rt << 1 | 1);
-        PushUp(rt);//更新本节点信息
-    }
-
-    /**
-     *下推lazy标记  同时更新当前层，清除当前层标记
-     * 查找{@link #Query}和更新{@link #Update}时使用
-     *
-     * */
-    void PushDown(int rt, int l, int r) {
-        //l
-        if (Add[rt] != 0) {
-            //下推标记  下层加上add标记
-            Add[rt << 1] += Add[rt];
-            Add[rt << 1 | 1] += Add[rt];
-
-            int m = (l + r) >> 1;
-            //下推后 更新sum值  并清除标记
-            //修改子节点的Sum使之与对应的Add相对应
-            Sum[rt << 1] += Add[rt] * (m - l + 1);
-            Sum[rt << 1 | 1] += Add[rt] * (r-m);
-            //清除本节点标记
-            Add[rt] = 0;
-        }
-    }
-
-    /**
-     * 区间查询
-     */
-    int Query(int L, int R, int l, int r, int rt) {//L,R表示查询区间，l,r表示当前节点区间，rt表示当前节点编号
-        if (L <= l && r <= R) {
-            //在区间内，直接返回   这个是结果的一部分   区间加法加起来
-            return Sum[rt];
-        }
-        int m = (l + r) >> 1;
-        //下推标记，否则Sum可能不正确
-        PushDown(rt, m - l + 1, r - m);
-
-        //累计答案
-        int ANS = 0;
-        //左侧部分
-        if (L <= m) ANS += Query(L, R, l, m, rt << 1);
-        //右侧部分
-        if (R > m) ANS += Query(L, R, m + 1, r, rt << 1 | 1);
-        return ANS;
+//      return void(tree[p] += d * (cr - cl + 1), mark[p] += d);
+        push_down(p, cr - cl + 1);
+        int mid = (cl + cr) >> 1;
+        if (mid >= l) update(l, r, d, p << 1, cl, mid);
+        if (mid < r) update(l, r, d, p << 1 | 1, mid + 1, cr);
+        tree[p] = tree[p << 1] + tree[p << 1 | 1];
     }
 }
-//todo  线段树 非数组实现
+
+
