d7f5bc3f710e31fa3f8ae87f5b10e9cab459b131
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-01-31 23:51:11 +0800

    计算器问题

--- src/main/java/com/dpz/template/API.java
+++ src/main/java/com/dpz/template/API.java
@@ -1,6 +1,6 @@
 package com.dpz.template;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class API {
 
@@ -149,5 +149,104 @@ public class API {
         return (int)res;
     }
 
+    //计算器   表达式问题   究极表达式问题
+    /*
+    | 从前往后做，对遍历到的字符做分情况讨论：
+    |
+    | 空格 : 跳过
+    | ( : 直接加入 ops 中，等待与之匹配的 )
+    | ) : 使用现有的 nums 和 ops 进行计算，直到遇到左边最近的一个左括号为止，计算结果放到 nums
+    | 数字 : 从当前位置开始继续往后取，将整一个连续数字整体取出，加入 nums
+    | + - | / ^ % : 需要将操作放入 ops 中。在放入之前先把栈内可以算的都算掉（只有「栈内运算符」
+    | 比「当前运算符」优先级高/同等，才进行运算），使用现有的 nums 和 ops 进行计算，直到没有操作或者遇到左括号，计算结果放到 nums
+    */
+    class Solution227 {
+        // 使用 map 维护一个运算符优先级
+        // 这里的优先级划分按照「数学」进行划分即可
+        Map<Character, Integer> map = new HashMap<>() {{
+            put('-', 1);
+            put('+', 1);
+            put('*', 2);
+            put('/', 2);
+            put('%', 2);
+            put('^', 3);//这里是指数运算
+        }};
+
+        public int calculate(String s) {
+            // 将所有的空格去掉
+            s = s.replaceAll(" ", "");
+            char[] cs = s.toCharArray();
+            int n = s.length();
+            // 存放所有的数字
+            Deque<Integer> nums = new ArrayDeque<>();
+            // 为了防止第一个数为负数，先往 nums 加个 0
+            nums.addLast(0);
+            // 存放所有「非数字以外」的操作
+            Deque<Character> ops = new ArrayDeque<>();
+            for (int i = 0; i < n; i++) {
+                char c = cs[i];
+                if (c == '(') {
+                    ops.addLast(c);
+                } else if (c == ')') {
+                    // 计算到最近一个左括号为止
+                    while (!ops.isEmpty()) {
+                        if (ops.peekLast() != '(') {
+                            calc(nums, ops);
+                        } else {
+                            ops.pollLast();
+                            break;
+                        }
+                    }
+                } else {
+                    if (isNumber(c)) {
+                        int u = 0;
+                        int j = i;
+                        // 将从 i 位置开始后面的连续数字整体取出，加入 nums
+                        while (j < n && isNumber(cs[j])) u = u * 10 + (cs[j++] - '0');
+                        nums.addLast(u);
+                        i = j - 1;
+                    } else {
+                        //防止括号内首个字符为运算符   "(-" 或 "(+"
+                        if (i > 0 && (cs[i - 1] == '(')) {
+                            nums.addLast(0);
+                        }
+                        // 有一个新操作要入栈时，先把栈内可以算的(优先级高或相同)都算了
+                        // 只有满足「栈内运算符」比「当前运算符」优先级高/同等，才进行运算
+                        while (!ops.isEmpty() && ops.peekLast() != '(') {
+                            char prev = ops.peekLast();
+                            if (map.get(prev) >= map.get(c)) {
+                                calc(nums, ops);
+                            } else {
+                                break;
+                            }
+                        }
+                        ops.addLast(c);
+                    }
+                }
+            }
+            // 将剩余的计算完
+            while (!ops.isEmpty()) calc(nums, ops);
+            return nums.peekLast();
+        }
+
+        void calc(Deque<Integer> nums, Deque<Character> ops) {
+            if (nums.size() < 2) return;
+            if (ops.isEmpty()) return;
+            int b = nums.pollLast(), a = nums.pollLast();
+            char op = ops.pollLast();
+            int ans = 0;
+            if (op == '+') ans = a + b;
+            else if (op == '-') ans = a - b;
+            else if (op == '*') ans = a * b;
+            else if (op == '/') ans = a / b;
+            else if (op == '^') ans = (int) Math.pow(a, b);
+            else if (op == '%') ans = a % b;
+            nums.addLast(ans);
+        }
+
+        boolean isNumber(char c) {
+            return Character.isDigit(c);
+        }
+    }
 
 }
