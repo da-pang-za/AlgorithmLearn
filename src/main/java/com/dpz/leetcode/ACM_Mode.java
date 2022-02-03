44815248335875a4b703998a41d44f9a4576857c
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-02-03 21:26:11 +0800

    ACM mode

--- src/main/java/com/dpz/leetcode/ACM_Mode.java
+++ src/main/java/com/dpz/leetcode/ACM_Mode.java
@@ -14,6 +14,8 @@ public class ACM_Mode {
             String likes=scanner.nextLine();
 
             System.out.println(n+","+m+","+cars+","+likes);
+
         }
     }
 }
+
---
+++ src/main/java/com/dpz/leetcode/Main.java
@@ -0,0 +1,48 @@
+package com.dpz.leetcode;
+
+//ACM mode
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T=in.nextInt();
+        for (int t = 0; t < T; t++) {
+            int A=in.nextInt();
+            int B=in.nextInt();
+            int C=in.nextInt();
+            double delta=Math.pow(A,2)-2*A*B*C;
+            if(delta<=0)
+                System.out.println(0);
+            else{
+                delta=Math.sqrt(delta);
+                double x1=(A-B*C+delta)/Math.pow(B,2);
+//                double x2=(A-B*C-delta)/Math.pow(B,2);
+                if(B>0){
+                    double e=1e-3;
+                    double area=0;
+                    for(double x=0;x<x1;x+=e){
+                        area+=Math.sqrt(2*A*x)-
+                                Math.max(B*x+C,-Math.sqrt(2*A*x));
+                    }
+                    System.out.println(area/1e3);
+                }
+                else {
+                    double e=1e-5;
+                    double area=0;
+                    for(double x=0;x<x1;x+=e){
+                        area+=Math.min(Math.sqrt(2*A*x),B*x+C) -
+                                -Math.sqrt(2*A*x);
+                    }
+                    System.out.println(area/1e3);
+                }
+
+            }
+        }
+
+    }
+
+}
