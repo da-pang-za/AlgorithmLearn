8c9e12857261ab26ba4e7642c3459f523dd01a69
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-03-22 15:41:07 +0800

    序列化 反序列化二叉树 (中序) 多叉树同理

--- src/main/java/com/dpz/dataStructure/TreeNode.java
+++ src/main/java/com/dpz/dataStructure/TreeNode.java
@@ -24,4 +24,52 @@ public class TreeNode {
     public String toString() {
         return ""+val;
     }
+    //中序遍历
+    static public class Codec {
+       static final String NULL="null";
+       static final String split=",";
+        // Encodes a tree to a single string.
+       static public String serialize(TreeNode root) {
+            if(root==null)return "null";
+            StringBuilder sb=new StringBuilder();
+            dfs1(root,sb);
+            sb.deleteCharAt(sb.length()-1);
+            return sb.toString();
+        }
+       static void dfs1(TreeNode root,StringBuilder sb){
+            if(root==null)return;
+            sb.append(root.val).append(split);
+            if(root.left==null)sb.append(NULL).append(split);
+            dfs1(root.left,sb);
+            if(root.right==null)sb.append(NULL).append(split);
+            dfs1(root.right,sb);
+        }
+
+        // Decodes your encoded data to tree.
+       static int index=0;
+       static public TreeNode deserialize(String data) {
+            String[] nodes = data.split(split);
+            index=0;
+            return dfs2(nodes);
+        }
+
+       static TreeNode dfs2(String[] nodes){
+            if(index>=nodes.length)return null;
+            if(nodes[index].equals(NULL)){
+                index++;
+                return null;
+            }
+            TreeNode root=new TreeNode(Integer.parseInt(nodes[index]));
+            index++;
+            root.left=dfs2(nodes);
+            root.right=dfs2(nodes);
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root=TreeNode.Codec.deserialize("1,2,null,null,3,4,null,null,5,null,null");
+
+        System.out.println(TreeNode.Codec.serialize(root));
+    }
 }
