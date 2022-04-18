4705efea4f4fe6ed31688ca24206018ff6ef2145
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-04-18 11:08:04 +0800

    fix

--- src/main/java/com/dpz/leetcode/ACM_Mode.java
+++ src/main/java/com/dpz/leetcode/ACM_Mode.java
@@ -1,21 +1,42 @@
 package com.dpz.leetcode;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Scanner;
 
+//public class ACM_Mode {
+//   static class Solution {
+//
+//        public static void main(String[] args) {
+//            Scanner scanner = new Scanner(System.in);
+//            // 读取 n m
+//            int n = scanner.nextInt(), m = scanner.nextInt();
+//            scanner.nextLine();
+//            String cars = scanner.nextLine();
+//            String likes=scanner.nextLine();
+//
+//            System.out.println(n+","+m+","+cars+","+likes);
+//
+//        }
+//    }
+//}
+
+
 public class ACM_Mode {
-   static class Solution {
+    static class Solution {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            // 读取 n m
-            int n = scanner.nextInt(), m = scanner.nextInt();
-            scanner.nextLine();
-            String cars = scanner.nextLine();
-            String likes=scanner.nextLine();
+        public static void main(String[] args) throws IOException {
 
-            System.out.println(n+","+m+","+cars+","+likes);
+//            Scanner scanner = new Scanner(new FileInputStream("" +
+//                    "/Users/dpz/工作 面试/LeetCode/sort/src/main/java/com/dpz/leetcode/Solution.java"));
+//            // 读取 n m
+//            while(scanner.hasNext()){
+//                System.out.println(scanner.nextLine());
+//            }
 
         }
     }
 }
-
