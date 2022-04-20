c8070846fc53b7bfcab908e4c5237c66d5d2c5ed
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-04-20 22:54:32 +0800

    线段树数组实现 * 增量更新 * 赋值更新

--- src/main/java/com/dpz/dataStructure/SegmentTree.java
+++ src/main/java/com/dpz/dataStructure/SegmentTree.java
@@ -1,8 +1,8 @@
 package com.dpz.dataStructure;
 //线段树讲解：https://zhuanlan.zhihu.com/p/106118909
 //例题：https://leetcode-cn.com/problems/range-sum-query-mutable/
-/**
- * todo 动态开点
+/*
+ * todo 动态开点 、Node实现、动态设置幺元
  * 线段树
  * 线段树的用处就是，对编号连续的一些点进行修改或者统计操作，修改和统计的复杂度都是O(log2(n)).
  * 线段树中，每个点代表一条线段（区间）
@@ -14,48 +14,14 @@ package com.dpz.dataStructure;
  * 最大值——总最大值=max(左区间最大值，右区间最大值)
  * 不符合区间加法的例子：
  * 众数——只知道左右区间的众数，没法求总区间的众数
+ * <p>
  */
 
-/**
- * 线段树 基础版
- */
-//class SegmentTree {
-//    int sum;
-//    int left;
-//    int right;
-//    SegmentTree lNode;
-//    SegmentTree rNode;
-//
-//    SegmentTree(int l, int r, int[] nums) {
-//        left = l;
-//        right = r;
-//
-//        if (l != r) {
-//            int mid = left + right >> 1;
-//            lNode = new SegmentTree(l, mid, nums);
-//            rNode = new SegmentTree(mid + 1, r, nums);
-//            sum = lNode.sum + rNode.sum;
-//        } else sum = nums[l];
-//    }
-//
-//    void add(int l, int r, int val) {
-//        if (l > right || r < left) return;
-//        if (left == right) sum += val;
-//        else {
-//            lNode.add(l, r, val);
-//            rNode.add(l, r, val);
-//            sum = lNode.sum + rNode.sum;
-//        }
-//    }
-//
-//    int query(int l, int r) {
-//        if (l > right || r < left) return 0;
-//        if (l <= left && right <= r) return sum;
-//        return lNode.query(l, r) + rNode.query(l, r);
-//    }
-//
-//}
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeSet;
 
 /**
  * 数组实现
@@ -63,21 +29,42 @@ package com.dpz.dataStructure;
  * https://leetcode-cn.com/problems/range-sum-query-mutable/solution/meng-xin-de-di-yi-ke-xian-duan-shu-by-98-c3es/
  */
 
-
+public
 class SegmentTree {
+    interface M {
+        long merge(long a, long b);
+    }
+
+    interface MN {
+        long mergeN(long a, long count);
+    }
+
+    static final int ADD = 0;//增量
+    static final int ASSIGN = 1;//赋值
     int MAXN;
-    int[] tree;
-    int[] lazy;
+    long[] tree;
+    long[] lazy;
+    boolean[] hasLazy;
     int[] nums;
     int n;
+    int mode;
+    M m;
+    MN mn;
 
-    SegmentTree(int[] A) {
-        n=MAXN = A.length;
+    SegmentTree(int[] A, int _n, M merge, MN mergeN, int mode) {
+        this.m = merge;
+        this.mn = mergeN;
+        this.mode = mode;
+        n = MAXN = _n;
         nums = A;
-        tree = new int[MAXN << 2];
-        lazy = new int[MAXN << 2];
-        build(1, 1, n);
+        tree = new long[MAXN << 2];
+        lazy = new long[MAXN << 2];
+        if (mode == ASSIGN)
+            hasLazy = new boolean[MAXN << 2];
+        if (A != null)
+            build(1, 1, n);
     }
+
     //p:节点idx
     void build(int p, int l, int r) {
         if (l == r) {
@@ -95,52 +82,101 @@ class SegmentTree {
         return query(l, r, 1, 1, n);
     }
 
-    long query(int l, int r, int p, int cl, int cr) {
-        if (cl >= l && cr <= r) return tree[p];
-        int mid = (cl + cr) >> 1;
-        push_down(p, cl, cr, mid);
+    long query(int l, int r, int p, int nl, int nr) {
+        if (nl >= l && nr <= r) return tree[p];
+        int mid = (nl + nr) >> 1;
+        push_down(p, nl, nr);
         long ans = 0;
-        if (mid >= l) ans = merge(ans, query(l, r, p << 1, cl, mid));
-        if (mid < r) ans = merge(ans, query(l, r, p << 1 | 1, mid + 1, cr));
+        if (mid >= l) ans = m.merge(ans, query(l, r, p << 1, nl, mid));
+        if (mid < r) ans = m.merge(ans, query(l, r, p << 1 | 1, mid + 1, nr));
         return ans;
     }
 
-    void update(int l, int r, int val) {
+
+    void update(int l, int r, long val) {
         update(l, r, val, 1, 1, n);
     }
 
-    //d 增量
-    void update(int l, int r, int d, int p, int cl, int cr) {
-        if (cl >= l && cr <= r) {
-            tree[p] += d * (cr - cl + 1);
-            lazy[p] += d;
+    void update(int l, int r, long d, int p, int nl, int nr) {
+        if (l <= nl && nr <= r) {
+            if (mode == ADD) {
+                tree[p] = m.merge(tree[p], mn.mergeN(d, nr - nl + 1));
+                lazy[p] = m.merge(lazy[p], d);
+            } else if (mode == ASSIGN) {
+                tree[p] = mn.mergeN(d, nr - nl + 1);
+                lazy[p] = d;
+                hasLazy[p] = true;
+            } else System.err.println("[SegmentTree]:unknown mode");
+
             return;
         }
-        int mid = (cl + cr) >> 1;
-        push_down(p, cl, cr, mid);
 
-        if (mid >= l) update(l, r, d, p << 1, cl, mid);
-        if (mid < r) update(l, r, d, p << 1 | 1, mid + 1, cr);
+        push_down(p, nl, nr);
+
+        int mid = (nl + nr) >> 1;
+        if (mid >= l) update(l, r, d, p << 1, nl, mid);
+        if (mid < r) update(l, r, d, p << 1 | 1, mid + 1, nr);
         push_up(p);
     }
+
     //利用左右点 更新当前点
     void push_up(int p) {
-        tree[p] = tree[p << 1] + tree[p << 1 | 1];
+        tree[p] = m.merge(tree[p << 1], tree[p << 1 | 1]);
     }
 
-    void push_down(int p, int l, int r, int mid) {
-        if (lazy[p] == 0) return;
-        lazy[p << 1] += lazy[p];
-        lazy[p << 1 | 1] += lazy[p];
-        tree[p << 1] += lazy[p] * (mid - l + 1);
-        tree[p << 1 | 1] += lazy[p] * (r - mid);
+    void push_down(int p, int l, int r) {
+        if (l >= r) return;
+        int mid = (l + r) >> 1;
+        if (mode == ADD) {
+            lazy[p << 1] = m.merge(lazy[p << 1], lazy[p]);
+            lazy[p << 1 | 1] = m.merge(lazy[p << 1 | 1], lazy[p]);
+
+            tree[p << 1] = m.merge(tree[p << 1], mn.mergeN(lazy[p], mid - l + 1));
+            tree[p << 1 | 1] = m.merge(tree[p << 1 | 1], mn.mergeN(lazy[p], r - mid));
+        } else if (mode == ASSIGN) {
+            if (!hasLazy[p]) return;
+            lazy[p << 1] = lazy[p];//此处如果当前节点没有标记  会把子节点的标记给擦除  但当前节点也可能有值为0的标记
+            lazy[p << 1 | 1] = lazy[p];
+            hasLazy[p << 1] = true;
+            hasLazy[p << 1 | 1] = true;
+            tree[p << 1] = mn.mergeN(lazy[p], mid - l + 1);
+            tree[p << 1 | 1] = mn.mergeN(lazy[p], r - mid);
+            hasLazy[p] = false;
+        } else System.err.println("[SegmentTree]:unknown mode");
+
         lazy[p] = 0;
     }
-
-    long merge(long a, long b) {
-        return a + b;
-    }
 }
+//demo:690.掉落的方块
+//class Solution {
+//    public List<Integer> fallingSquares(int[][] positions) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        int idx = 0;
+//        TreeSet<Integer> set = new TreeSet<>();
+//        for (int[] p : positions) {
+//            set.add(p[0]);
+//            set.add(p[1] + p[0] - 1);
+//        }
+//        for (Integer v : set) {
+//            map.put(v, ++idx);
+//        }
+//        int n = idx;
+//        SegmentTree segmentTree = new SegmentTree(null, n, (a, b) -> Math.max(a, b), (a, b) -> a, SegmentTree.ASSIGN);
+//        List<Integer> ans = new ArrayList<>();
+//        for (int[] p : positions) {
+//            int l = map.get(p[0]);
+//            int r = map.get(p[0] + p[1] - 1);
+//
+//            //当前范围最高的
+//            int h = (int) segmentTree.query(l, r);
+//            segmentTree.update(l, r, h + p[1]);
+//            ans.add((int) segmentTree.query(1, n));
+//        }
+//        return ans;
+//    }
+//}
+
+
 
 
 
