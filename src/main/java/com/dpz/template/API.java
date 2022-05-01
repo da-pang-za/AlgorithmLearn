1a3c8f17081b2cf0cef6b37c6dbc6dcdce3ce1c3
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-05-01 17:52:33 +0800

    todo

--- src/main/java/com/dpz/template/API.java
+++ src/main/java/com/dpz/template/API.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class API {
+    //todo 三进制
 
     //二分查找
     //第一个>=x的元素的idx(下标)
@@ -464,4 +465,42 @@ public class API {
         arr[j] = temp;
     }
 
+
+    //排序
+
+    public static int[] radixSort2(int[] a) {
+        int n = a.length;
+        int[] c0 = new int[0x101];
+        int[] c1 = new int[0x101];
+        int[] c2 = new int[0x101];
+        int[] c3 = new int[0x101];
+        for (int v : a) {
+            c0[(v & 0xff) + 1]++;
+            c1[(v >>> 8 & 0xff) + 1]++;
+            c2[(v >>> 16 & 0xff) + 1]++;
+            c3[(v >>> 24 ^ 0x80) + 1]++;
+        }
+        for (int i = 0; i < 0xff; i++) {
+            c0[i + 1] += c0[i];
+            c1[i + 1] += c1[i];
+            c2[i + 1] += c2[i];
+            c3[i + 1] += c3[i];
+        }
+        int[] t = new int[n];
+        for (int v : a) t[c0[v & 0xff]++] = v;
+        for (int v : t) a[c1[v >>> 8 & 0xff]++] = v;
+        for (int v : a) t[c2[v >>> 16 & 0xff]++] = v;
+        for (int v : t) a[c3[v >>> 24 ^ 0x80]++] = v;
+        return a;
+    }
+
+    static void sortWithPQ(int[] a) {
+        sortWithPQ(a, 0, a.length - 1);
+    }
+
+    static void sortWithPQ(int[] a, int start, int end) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = start; i <= end; i++) pq.add(a[i]);
+        for (int i = start; i <= end; i++) a[i] = pq.poll();
+    }
 }
