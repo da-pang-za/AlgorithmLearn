07e8ce1477a016eb124db4e7851fc5cf90dd78e5
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-06-27 17:44:10 +0800

    双链表

--- src/main/java/com/dpz/dataStructure/ListNode.java
+++ src/main/java/com/dpz/dataStructure/ListNode.java
@@ -4,28 +4,34 @@ package com.dpz.dataStructure;
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode(){}
+    public ListNode pre;
+
+    public ListNode() {
+    }
+
     public ListNode(int x) {
         val = x;
     }
-    public ListNode(int[]nums){
-        assert nums.length>0;
-        this.val=nums[0];
-        ListNode node=this;
-        for(int i=1;i<nums.length;i++){
-            node.next=new ListNode(nums[i]);
-            node=node.next;
+
+    public ListNode(int[] nums) {
+        assert nums.length > 0;
+        this.val = nums[0];
+        ListNode node = this;
+        for (int i = 1; i < nums.length; i++) {
+            node.next = new ListNode(nums[i]);
+            node.next.pre = node;
+            node = node.next;
         }
 
     }
 
     @Override
     public String toString() {
-        StringBuilder sb=new StringBuilder();
-        ListNode node=this;
-        while (node!=null){
+        StringBuilder sb = new StringBuilder();
+        ListNode node = this;
+        while (node != null) {
             sb.append(node.val).append("->");
-            node=node.next;
+            node = node.next;
         }
         return sb.toString();
     }
