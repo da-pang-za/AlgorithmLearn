45ca0b963b888e5adc2e26f1ced00e8417ff86a9
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-06-29 22:24:26 +0800

    字典树 fix 01字典树

--- src/main/java/com/dpz/dataStructure/Trie.java
+++ src/main/java/com/dpz/dataStructure/Trie.java
@@ -4,47 +4,83 @@ package com.dpz.dataStructure;
  * 字典树 or 前缀树
  * word仅有小写英文字母组成
  */
-public class Trie {
+public
+class Trie {
 
-    Trie[]nodes;
-    boolean flag;//是否为有效的单词结尾
+    Trie[] nodes;
+    int cnt;//当前结尾代表的单词的个数
+
     public Trie() {
-        nodes=new Trie[26];
-        flag=false;
+        nodes = new Trie[26];
+        cnt = 0;
 
     }
 
     public void insert(String word) {
-        Trie cur=this;
+        Trie cur = this;
         for (char c : word.toCharArray()) {
-            if(cur.nodes[c-'a']==null){
-                cur.nodes[c-'a']=new Trie();
+            if (cur.nodes[c - 'a'] == null) {
+                cur.nodes[c - 'a'] = new Trie();
             }
-            cur=cur.nodes[c-'a'];
+            cur = cur.nodes[c - 'a'];
         }
-        cur.flag=true;
+        cur.cnt++;
     }
 
-    public boolean search(String word) {
-        Trie cur=this;
+    public int search(String word) {
+        Trie cur = this;
 
         for (char c : word.toCharArray()) {
-            if(cur.nodes[c-'a']==null)return false;
-            cur=cur.nodes[c-'a'];
+            if (cur.nodes[c - 'a'] == null) return 0;
+            cur = cur.nodes[c - 'a'];
         }
-        return cur.flag;
+        return cur.cnt;
 
     }
 
     //判断前缀是否存在
     public boolean startsWith(String prefix) {
-        Trie cur=this;
+        Trie cur = this;
 
         for (char c : prefix.toCharArray()) {
-            if(cur.nodes[c-'a']==null)return false;
-            cur=cur.nodes[c-'a'];
+            if (cur.nodes[c - 'a'] == null) return false;
+            cur = cur.nodes[c - 'a'];
         }
         return true;
     }
 }
 
+//01字典树
+class Trie01 {
+
+    Trie01[] nodes;
+    int cnt;//当前结尾代表的单词的个数
+
+    public Trie01() {
+        nodes = new Trie01[2];
+        cnt = 0;
+    }
+
+    public void insert(int v) {
+        Trie01 cur = this;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (v >> i) & 1;
+            if (cur.nodes[bit] == null) {
+                cur.nodes[bit] = new Trie01();
+            }
+            cur = cur.nodes[bit];
+        }
+        cur.cnt++;
+    }
+
+    public int search(int v) {
+        Trie01 cur = this;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (v >> i) & 1;
+            if (cur.nodes[bit] == null) return 0;
+            cur = cur.nodes[bit];
+        }
+        return cur.cnt;
+    }
+
+}
