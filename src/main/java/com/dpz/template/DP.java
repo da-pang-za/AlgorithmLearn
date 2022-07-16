6a9a50c4013cded20ba77bcc6c7bb930c89e5e1e
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-07-16 17:31:28 +0800

    dp

--- src/main/java/com/dpz/template/DP.java
+++ src/main/java/com/dpz/template/DP.java
@@ -3,7 +3,7 @@ package com.dpz.template;
 //背包问题
 public class DP {
 
-    public static class Backpack {
+    static class Backpack {
         //    //总容量不超过V的情况下的最大价值
 //    int N = ni(), V = ni();
 //    int[] v = new int[N];//容量
@@ -92,29 +92,82 @@ public class DP {
             }
         }
     }
-}
 
-//区间DP
-class Interval {
-    //https://www.acwing.com/problem/content/284/
-    static int acwing282(int n, int[] nums) {
-        int[] sum = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            sum[i + 1] = sum[i] + nums[i];
-        }
-        int[][] dp = new int[n][n];//区间[i,j]合并为一个的最小代价
+    //区间DP
+    static class Interval {
+        //https://www.acwing.com/problem/content/284/
+        static int acwing282(int n, int[] nums) {
+            int[] sum = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                sum[i + 1] = sum[i] + nums[i];
+            }
+            int[][] dp = new int[n][n];//区间[i,j]合并为一个的最小代价
 
-        for (int len = 2; len <= n; len++) {
-            for (int l = 0, r = l + len - 1; r < n; l++, r++) {
-                dp[l][r] = Integer.MAX_VALUE;
-                //[u,r]合并为一堆
-                for (int u = l + 1; u <= r; u++) {
-                    dp[l][r] = Math.min(dp[l][r], dp[l][u - 1] + dp[u][r] + sum[r + 1] - sum[l]);
+            for (int len = 2; len <= n; len++) {
+                for (int l = 0, r = l + len - 1; r < n; l++, r++) {
+                    dp[l][r] = Integer.MAX_VALUE;
+                    //[u,r]合并为一堆
+                    for (int u = l + 1; u <= r; u++) {
+                        dp[l][r] = Math.min(dp[l][r], dp[l][u - 1] + dp[u][r] + sum[r + 1] - sum[l]);
+                    }
                 }
             }
+            return dp[0][n - 1];
         }
-        return dp[0][n - 1];
     }
+
+    //记忆化搜索
+    //https://www.acwing.com/problem/content/903/
+    static int maxPath(int[][] grid) {
+        int max = 0;
+        int m = grid.length, n = grid[0].length;
+        Integer[][] dp = new Integer[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, dfs903(grid, i, j, dp));
+            }
+        }
+        return max;
+    }
+
+    private static int dfs903(int[][] grid, int i, int j, Integer[][] dp) {
+        if (dp[i][j] == null) {
+            int h = grid[i][j];
+            int max = 0;
+            if (i - 1 >= 0 && grid[i - 1][j] < h) max = Math.max(max, dfs903(grid, i - 1, j, dp));
+            if (j - 1 >= 0 && grid[i][j - 1] < h) max = Math.max(max, dfs903(grid, i, j - 1, dp));
+            if (i + 1 < grid.length && grid[i + 1][j] < h) max = Math.max(max, dfs903(grid, i + 1, j, dp));
+            if (j + 1 < grid[i].length && grid[i][j + 1] < h) max = Math.max(max, dfs903(grid, i, j + 1, dp));
+            dp[i][j] = max + 1;
+        }
+        return dp[i][j];
+    }
+
+    //树形DP
+    //https://www.acwing.com/problem/content/287/
+
+
+    //编辑距离
+    static public int minDistance(String word1, String word2) {
+        int m=word1.length(),n=word2.length();
+        int[][]dp=new int[m+1][n+1];
+        for(int i=1;i<=m;i++)dp[i][0]=i;
+        for(int j=1;j<=n;j++)dp[0][j]=j;
+        for(int i=1;i<=m;i++){
+            char a=word1.charAt(i-1);
+            for(int j=1;j<=n;j++){
+                char b=word2.charAt(j-1);
+                if(a==b)dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+            }
+        }
+        return dp[m][n];
+    }
+
+
+
 }
+
+
 
 
