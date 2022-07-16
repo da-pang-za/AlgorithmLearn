f03e246d24a68d6b4cd730b78d4ef174545a3407
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-07-16 21:52:05 +0800

    nothing

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -5,56 +5,40 @@ import java.util.PriorityQueue;
 
 
 class Solution {
-    public int countPalindromicSubsequences(String s) {
-        final int MOD = 1000000007;
-        int n = s.length();
-        int[][] dp = new int[n][n];
-        int[][] next = new int[n][4];
-        int[][] pre = new int[n][4];
 
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = 1;
+    int min;
+
+    public int minimumScore(int[] nums, int[][] edges) {
+        ArrayList<Integer>[] adj = new ArrayList[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            adj[i] = new ArrayList<>();
         }
-
-        int[] pos = new int[4];
-        Arrays.fill(pos, -1);
-
-        for (int i = 0; i < n; i++) {
-            for (int c = 0; c < 4; c++) {
-                pre[i][c] = pos[c];
-            }
-            pos[s.charAt(i) - 'a'] = i;
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
         }
-
-        pos[0] = pos[1] = pos[2] = pos[3] = n;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int c = 0; c < 4; c++) {
-                next[i][c] = pos[c];
-            }
-            pos[s.charAt(i) - 'a'] = i;
+        int ans = Integer.MAX_VALUE;
+        for (int[] edge : edges) {
+            int xor0 = dfs(edge[0], edge[1], 0, 0, nums, adj),
+                    xor1 = dfs(edge[1], edge[0], 0, 0, nums, adj);
+            min = Integer.MAX_VALUE;
+            dfs(edge[0], edge[1], xor0, xor1, nums, adj);
+            dfs(edge[1], edge[0], xor1, xor0, nums, adj);
+            ans = Math.min(ans, min);
         }
+        return ans;
+    }
 
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len <= n; i++) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    int low = next[i][s.charAt(i) - 'a'];
-                    int high = pre[j][s.charAt(i) - 'a'];
-                    if (low > high) {
-                        dp[i][j] = (2 + dp[i + 1][j - 1] * 2) % MOD;
-                    } else if (low == high) {
-                        dp[i][j] = (1 + dp[i + 1][j - 1] * 2) % MOD;
-                    } else {
-                        dp[i][j] = (dp[i + 1][j - 1] * 2 % MOD - dp[low + 1][high - 1] + MOD) % MOD;
-                    }
-                } else {
-                    dp[i][j] = ((dp[i + 1][j] + dp[i][j - 1]) % MOD - dp[i + 1][j - 1] + MOD) % MOD;
-                }
+    private int dfs(int i, int parent, int xor0, int xor1, int[] nums, ArrayList<Integer>[] list) {
+        int xor = nums[i];
+        for (int j : list[i]) {
+            if (j != parent) {
+                int curr = dfs(j, i, xor0, xor1, nums, list);
+                min = Math.min(min,
+                        Math.max(curr, Math.max(curr ^ xor0, xor1)) - Math.min(curr, Math.min(curr ^ xor0, xor1)));
+                xor ^= curr;
             }
         }
-
-        return dp[0][n - 1];
+        return xor;
     }
 }
-
