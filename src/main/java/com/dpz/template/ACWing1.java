313b80e3bf8a97ae2b8373966482130e85ff27f0
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-07-30 14:00:41 +0800

    算法提高课

---
+++ src/main/java/com/dpz/template/ACWing1.java
@@ -0,0 +1,39 @@
+package com.dpz.template;
+
+/**
+ * ACWing算法模板  算法提高课
+ */
+public class ACWing1 {
+
+    /**
+     * 基础算法
+     */
+    static class Basic{}
+    /**
+     * 数学
+     */
+    static class Math1{}
+    /**
+     * 数据结构
+     */
+    static class DataStructure{}
+    /**
+     * 贪心
+     */
+    static class Greedy{}
+    /**
+     * 动态规划 DP
+     *
+     * @see DP
+     */
+    static class DP1 {
+
+    }
+    /**
+     * 图论
+     *
+     * @see Graph
+     */
+    static class G{}
+
+}
