aa3319c6949f1bd51668ba532542806ca7a1d40c
dpz <2500018898@qq.com>
2022-08-04 20:47:10 +0800

    fix

--- src/main/java/com/dpz/template/DP.java
+++ src/main/java/com/dpz/template/DP.java
@@ -35,6 +35,9 @@ public class DP {
                 return dp[N][V];
             }
 
+            /**
+             * 一维优化 注意内层驯化是倒序的
+             */
             static int _01bp1d(int N, int V, int[] v, int[] w) {
                 //总容量不超过V的情况下的最大价值
                 int[] dp = new int[V + 1];//前i个物品 容量不超过j的最大价值
@@ -149,34 +152,30 @@ public class DP {
 
     //编辑距离
     static public int minDistance(String word1, String word2) {
-        int m=word1.length(),n=word2.length();
-        int[][]dp=new int[m+1][n+1];
-        for(int i=1;i<=m;i++)dp[i][0]=i;
-        for(int j=1;j<=n;j++)dp[0][j]=j;
-        for(int i=1;i<=m;i++){
-            char a=word1.charAt(i-1);
-            for(int j=1;j<=n;j++){
-                char b=word2.charAt(j-1);
-                if(a==b)dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) dp[i][0] = i;
+        for (int j = 1; j <= n; j++) dp[0][j] = j;
+        for (int i = 1; i <= m; i++) {
+            char a = word1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char b = word2.charAt(j - 1);
+                if (a == b) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
             }
         }
         return dp[m][n];
     }
 
-    //=======================算法提高课===========================
+    //状压DP
+    static class BitMask {
+        //蒙德里安的梦想
+        //https://www.acwing.com/activity/content/problem/content/1010/
+        static void go() {
+            //不含连续奇数0的状态   预处理
+            boolean[] valid = new boolean[1 << 11];
 
-}
-
-//状压DP
-class BitMask{
-    //蒙德里安的梦想
-    //https://www.acwing.com/activity/content/problem/content/1010/
-    static void go() {
-        //不含连续奇数0的状态   预处理
-        boolean[] valid = new boolean[1 << 11];
-
-        int N=11, M=10;
+            int N = 11, M = 10;
 
             for (int i = 0; i < 1 << 11; i++) {
                 valid[i] = true;
@@ -203,10 +202,14 @@ class BitMask{
             }
             System.out.println(dp[N][0]);
 
-    }
+        }
 
+
+    }
+    //=======================算法提高课===========================
 
 }
+
 
 
 
