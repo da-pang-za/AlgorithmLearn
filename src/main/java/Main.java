f0982f2078a9ddda3dfa9d923350f047f03d4d19
dpz <2500018898@qq.com>
2022-08-26 13:21:32 +0800

    增加对拍 TEST为true时进行对拍 分别输出到文件 TEST为false时正常标准输出

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,29 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 
 public class Main {
-    static private final boolean oj = !local();
-
-    String INPUT = "";
-
-    void solve() {
-        for (int T = 1; T > 0; T--) go();
-    }
 
     //#0.00代表保留两位小数
-    DecimalFormat df = new DecimalFormat("#0.000000");
+    static DecimalFormat df = new DecimalFormat("#0.00000000");
+
+    void solve() {
+        for (int T = 1; T > 0; T--) {
+            go();
+            goAC(out);
+        }
+    }
 
     void go() {
+        for (int i = 0; i < 10; i++) {
+            out.println(i*i);
+        }
+    }
+
+    void go1() {
 
     }
+
+    boolean TEST = false;//对拍  只需要改这里
+
 
     //===================== MAIN =============================
     public static void main(String[] args) throws Exception {
@@ -25,49 +34,41 @@ public class Main {
     }
 
     void run() throws Exception {
-        long S = System.currentTimeMillis();
         if (INPUT.length() > 0)
             is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
         else is = oj ? System.in : new ByteArrayInputStream(new FileInputStream("input/a.test").readAllBytes());
-
-        out = new PrintWriter(System.out);
-
+        out = new UWI.FastWriter(System.out);
+        if(oj)TEST=false;
+        if (TEST) {
+            out = testRun;
+            testRun.print("");
+            testAC.print("");
+        }
+        long s = System.currentTimeMillis();
         solve();
         out.flush();
-        long G = System.currentTimeMillis();
-        tr(G - S + "ms 提交时记得清空INPUT！");
+        testRun.flush();
+        testAC.flush();
+        tr(System.currentTimeMillis() - s + "ms");
     }
 
-    static InputStream is;
-    static PrintWriter out;
-
-
-    private static boolean eof() {
-        if (lenbuf == -1) return true;
-        int lptr = ptrbuf;
-        while (lptr < lenbuf) if (!isSpaceChar(inbuf[lptr++])) return false;
-
-        try {
-            is.mark(1000);
-            while (true) {
-                int b = is.read();
-                if (b == -1) {
-                    is.reset();
-                    return true;
-                } else if (!isSpaceChar(b)) {
-                    is.reset();
-                    return false;
-                }
-            }
-        } catch (IOException e) {
-            return true;
-        }
+    void goAC(UWI.FastWriter pre) {
+        if(!TEST)return;
+        out = testAC;
+        go1();
+        out=pre;
     }
+
+    InputStream is;
+    UWI.FastWriter out;
+    UWI.FastWriter testRun = new UWI.FastWriter("output/run.out");
+    UWI.FastWriter testAC = new UWI.FastWriter("output/ac.out");
+    String INPUT = "";
 
     private static byte[] inbuf = new byte[1024];
     static int lenbuf = 0, ptrbuf = 0;
 
-    private static int readByte() {
+    private  int readByte() {
         if (lenbuf == -1) throw new InputMismatchException();
         if (ptrbuf >= lenbuf) {
             ptrbuf = 0;
@@ -81,26 +82,26 @@ public class Main {
         return inbuf[ptrbuf++];
     }
 
-    private static boolean isSpaceChar(int c) {
+    private  boolean isSpaceChar(int c) {
         return !(c >= 33 && c <= 126);
     }
 
     //	private static boolean isSpaceChar(int c) { return !(c >= 32 && c <= 126); }
-    private static int skip() {
+    private  int skip() {
         int b;
         while ((b = readByte()) != -1 && isSpaceChar(b)) ;
         return b;
     }
 
-    private static double nd() {
+    private  double nd() {
         return Double.parseDouble(ns());
     }
 
-    private static char nc() {
+    private  char nc() {
         return (char) skip();
     }
 
-    private static String ns() {
+    private  String ns() {
         int b = skip();
         StringBuilder sb = new StringBuilder();
         while (!(isSpaceChar(b))) {
@@ -110,7 +111,7 @@ public class Main {
         return sb.toString();
     }
 
-    private static char[] ns(int n) {
+    private  char[] ns(int n) {
         char[] buf = new char[n];
         int b = skip(), p = 0;
         while (p < n && !(isSpaceChar(b))) {
@@ -120,13 +121,13 @@ public class Main {
         return n == p ? buf : Arrays.copyOf(buf, p);
     }
 
-    private static char[][] nm(int n, int m) {
+    private  char[][] nm(int n, int m) {
         char[][] map = new char[n][];
         for (int i = 0; i < n; i++) map[i] = ns(m);
         return map;
     }
 
-    private static int[] na(int n) {
+    private  int[] na(int n) {
         int[] a = new int[n];
         for (int i = 0; i < n; i++) a[i] = ni();
         return a;
@@ -138,12 +139,12 @@ public class Main {
         return a;
     }
 
-    private static int ni() {
+    private  int ni() {
         return (int) nl();
     }
 
     //long
-    private static long nl() {
+    private  long nl() {
         long num = 0;
         int b;
         boolean minus = false;
@@ -175,4 +176,6 @@ public class Main {
         }
         return false;
     }
+    static private final boolean oj = !local();
+
 }
