ac2b1fe70c20f0504332cf523b0af89fb465c5f7
dpz <2500018898@qq.com>
2022-08-26 15:20:58 +0800

    增加对拍 TEST为true时进行对拍 分别输出到文件 TEST为false时正常标准输出

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,7 @@ public class Main {
     }
 
     void go() {
-        for (int i = 0; i < 10; i++) {
-            out.println(i*i);
-        }
+
     }
 
     void go1() {
@@ -37,9 +35,11 @@ public class Main {
         if (INPUT.length() > 0)
             is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
         else is = oj ? System.in : new ByteArrayInputStream(new FileInputStream("input/a.test").readAllBytes());
-        out = new UWI.FastWriter(System.out);
-        if(oj)TEST=false;
+        out = new FastWriter(System.out);
+        if (oj) TEST = false;
         if (TEST) {
+            testRun = new FastWriter("output/run.out");
+            testAC = new FastWriter("output/ac.out");
             out = testRun;
             testRun.print("");
             testAC.print("");
@@ -47,28 +47,30 @@ public class Main {
         long s = System.currentTimeMillis();
         solve();
         out.flush();
-        testRun.flush();
-        testAC.flush();
+        if (TEST) {
+            testRun.flush();
+            testAC.flush();
+        }
         tr(System.currentTimeMillis() - s + "ms");
     }
 
-    void goAC(UWI.FastWriter pre) {
-        if(!TEST)return;
+    void goAC(FastWriter pre) {
+        if (!TEST) return;
         out = testAC;
         go1();
-        out=pre;
+        out = pre;
     }
 
     InputStream is;
-    UWI.FastWriter out;
-    UWI.FastWriter testRun = new UWI.FastWriter("output/run.out");
-    UWI.FastWriter testAC = new UWI.FastWriter("output/ac.out");
+    FastWriter out;
+    FastWriter testRun;
+    FastWriter testAC;
     String INPUT = "";
 
     private static byte[] inbuf = new byte[1024];
     static int lenbuf = 0, ptrbuf = 0;
 
-    private  int readByte() {
+    private int readByte() {
         if (lenbuf == -1) throw new InputMismatchException();
         if (ptrbuf >= lenbuf) {
             ptrbuf = 0;
@@ -82,26 +84,26 @@ public class Main {
         return inbuf[ptrbuf++];
     }
 
-    private  boolean isSpaceChar(int c) {
+    private boolean isSpaceChar(int c) {
         return !(c >= 33 && c <= 126);
     }
 
     //	private static boolean isSpaceChar(int c) { return !(c >= 32 && c <= 126); }
-    private  int skip() {
+    private int skip() {
         int b;
         while ((b = readByte()) != -1 && isSpaceChar(b)) ;
         return b;
     }
 
-    private  double nd() {
+    private double nd() {
         return Double.parseDouble(ns());
     }
 
-    private  char nc() {
+    private char nc() {
         return (char) skip();
     }
 
-    private  String ns() {
+    private String ns() {
         int b = skip();
         StringBuilder sb = new StringBuilder();
         while (!(isSpaceChar(b))) {
@@ -111,7 +113,7 @@ public class Main {
         return sb.toString();
     }
 
-    private  char[] ns(int n) {
+    private char[] ns(int n) {
         char[] buf = new char[n];
         int b = skip(), p = 0;
         while (p < n && !(isSpaceChar(b))) {
@@ -121,13 +123,13 @@ public class Main {
         return n == p ? buf : Arrays.copyOf(buf, p);
     }
 
-    private  char[][] nm(int n, int m) {
+    private char[][] nm(int n, int m) {
         char[][] map = new char[n][];
         for (int i = 0; i < n; i++) map[i] = ns(m);
         return map;
     }
 
-    private  int[] na(int n) {
+    private int[] na(int n) {
         int[] a = new int[n];
         for (int i = 0; i < n; i++) a[i] = ni();
         return a;
@@ -139,12 +141,12 @@ public class Main {
         return a;
     }
 
-    private  int ni() {
+    private int ni() {
         return (int) nl();
     }
 
     //long
-    private  long nl() {
+    private long nl() {
         long num = 0;
         int b;
         boolean minus = false;
@@ -176,6 +178,299 @@ public class Main {
         }
         return false;
     }
+
     static private final boolean oj = !local();
 
+    static class FastWriter {
+        private static final int BUF_SIZE = 1 << 13;
+        private final byte[] buf = new byte[BUF_SIZE];
+        private final OutputStream out;
+        private int ptr = 0;
+
+        private FastWriter() {
+            out = null;
+        }
+
+        public FastWriter(OutputStream os) {
+            this.out = os;
+        }
+
+        public FastWriter(String path) {
+            try {
+                this.out = new FileOutputStream(path);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException("FastWriter");
+            }
+        }
+
+        public FastWriter write(byte b) {
+            buf[ptr++] = b;
+            if (ptr == BUF_SIZE) innerflush();
+            return this;
+        }
+
+        public FastWriter write(char c) {
+            return write((byte) c);
+        }
+
+        public FastWriter write(char[] s) {
+            for (char c : s) {
+                buf[ptr++] = (byte) c;
+                if (ptr == BUF_SIZE) innerflush();
+            }
+            return this;
+        }
+
+        public FastWriter write(String s) {
+            s.chars().forEach(c -> {
+                buf[ptr++] = (byte) c;
+                if (ptr == BUF_SIZE) innerflush();
+            });
+            return this;
+        }
+
+        private static int countDigits(int l) {
+            if (l >= 1000000000) return 10;
+            if (l >= 100000000) return 9;
+            if (l >= 10000000) return 8;
+            if (l >= 1000000) return 7;
+            if (l >= 100000) return 6;
+            if (l >= 10000) return 5;
+            if (l >= 1000) return 4;
+            if (l >= 100) return 3;
+            if (l >= 10) return 2;
+            return 1;
+        }
+
+        public FastWriter write(int x) {
+            if (x == Integer.MIN_VALUE) {
+                return write((long) x);
+            }
+            if (ptr + 12 >= BUF_SIZE) innerflush();
+            if (x < 0) {
+                write((byte) '-');
+                x = -x;
+            }
+            int d = countDigits(x);
+            for (int i = ptr + d - 1; i >= ptr; i--) {
+                buf[i] = (byte) ('0' + x % 10);
+                x /= 10;
+            }
+            ptr += d;
+            return this;
+        }
+
+        private static int countDigits(long l) {
+            if (l >= 1000000000000000000L) return 19;
+            if (l >= 100000000000000000L) return 18;
+            if (l >= 10000000000000000L) return 17;
+            if (l >= 1000000000000000L) return 16;
+            if (l >= 100000000000000L) return 15;
+            if (l >= 10000000000000L) return 14;
+            if (l >= 1000000000000L) return 13;
+            if (l >= 100000000000L) return 12;
+            if (l >= 10000000000L) return 11;
+            if (l >= 1000000000L) return 10;
+            if (l >= 100000000L) return 9;
+            if (l >= 10000000L) return 8;
+            if (l >= 1000000L) return 7;
+            if (l >= 100000L) return 6;
+            if (l >= 10000L) return 5;
+            if (l >= 1000L) return 4;
+            if (l >= 100L) return 3;
+            if (l >= 10L) return 2;
+            return 1;
+        }
+
+        public FastWriter write(long x) {
+            if (x == Long.MIN_VALUE) {
+                return write("" + x);
+            }
+            if (ptr + 21 >= BUF_SIZE) innerflush();
+            if (x < 0) {
+                write((byte) '-');
+                x = -x;
+            }
+            int d = countDigits(x);
+            for (int i = ptr + d - 1; i >= ptr; i--) {
+                buf[i] = (byte) ('0' + x % 10);
+                x /= 10;
+            }
+            ptr += d;
+            return this;
+        }
+
+        public FastWriter write(double x, int precision) {
+            if (x < 0) {
+                write('-');
+                x = -x;
+            }
+            x += Math.pow(10, -precision) / 2;
+            //		if(x < 0){ x = 0; }
+            write((long) x).write(".");
+            x -= (long) x;
+            for (int i = 0; i < precision; i++) {
+                x *= 10;
+                write((char) ('0' + (int) x));
+                x -= (int) x;
+            }
+            return this;
+        }
+
+        public FastWriter writeln(char c) {
+            return write(c).writeln();
+        }
+
+        public FastWriter writeln(int x) {
+            return write(x).writeln();
+        }
+
+        public FastWriter writeln(long x) {
+            return write(x).writeln();
+        }
+
+        public FastWriter writeln(double x, int precision) {
+            return write(x, precision).writeln();
+        }
+
+        public FastWriter write(int... xs) {
+            boolean first = true;
+            for (int x : xs) {
+                if (!first) write(' ');
+                first = false;
+                write(x);
+            }
+            return this;
+        }
+
+        public FastWriter write(long... xs) {
+            boolean first = true;
+            for (long x : xs) {
+                if (!first) write(' ');
+                first = false;
+                write(x);
+            }
+            return this;
+        }
+
+        public FastWriter writeln() {
+            return write((byte) '\n');
+        }
+
+        public FastWriter writeln(int... xs) {
+            return write(xs).writeln();
+        }
+
+        public FastWriter writeln(long... xs) {
+            return write(xs).writeln();
+        }
+
+        public FastWriter writeln(char[] line) {
+            return write(line).writeln();
+        }
+
+        public FastWriter writeln(char[]... map) {
+            for (char[] line : map) write(line).writeln();
+            return this;
+        }
+
+        public FastWriter writeln(String s) {
+            return write(s).writeln();
+        }
+
+        private void innerflush() {
+            try {
+                out.write(buf, 0, ptr);
+                ptr = 0;
+            } catch (IOException e) {
+                throw new RuntimeException("innerflush");
+            }
+        }
+
+        public void flush() {
+            innerflush();
+            try {
+                out.flush();
+            } catch (IOException e) {
+                throw new RuntimeException("flush");
+            }
+        }
+
+        public FastWriter print(byte b) {
+            return write(b);
+        }
+
+        public FastWriter print(char c) {
+            return write(c);
+        }
+
+        public FastWriter print(char[] s) {
+            return write(s);
+        }
+
+        public FastWriter print(String s) {
+            return write(s);
+        }
+
+        public FastWriter print(int x) {
+            return write(x);
+        }
+
+        public FastWriter print(long x) {
+            return write(x);
+        }
+
+        public FastWriter print(double x, int precision) {
+            return write(x, precision);
+        }
+
+        public FastWriter println(char c) {
+            return writeln(c);
+        }
+
+        public FastWriter println(int x) {
+            return writeln(x);
+        }
+
+        public FastWriter println(long x) {
+            return writeln(x);
+        }
+
+        public FastWriter println(double x, int precision) {
+            return writeln(x, precision);
+        }
+
+        public FastWriter print(int... xs) {
+            return write(xs);
+        }
+
+        public FastWriter print(long... xs) {
+            return write(xs);
+        }
+
+        public FastWriter println(int... xs) {
+            return writeln(xs);
+        }
+
+        public FastWriter println(long... xs) {
+            return writeln(xs);
+        }
+
+        public FastWriter println(char[] line) {
+            return writeln(line);
+        }
+
+        public FastWriter println(char[]... map) {
+            return writeln(map);
+        }
+
+        public FastWriter println(String s) {
+            return writeln(s);
+        }
+
+        public FastWriter println() {
+            return writeln();
+        }
+    }
 }
+
