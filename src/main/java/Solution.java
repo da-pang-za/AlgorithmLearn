2d401b00844dd598c9e61a460f37ba39a9d7922d
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-09-04 12:40:56 +0800

    LC 模板

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,18 +1,4 @@
-import java.util.*;
-
-
 class Solution {
-    public List<Integer> minSubsequence(int[] nums) {
-        int sum= Arrays.stream(nums).sum();
-        Arrays.sort(nums);
-        List<Integer>ans=new ArrayList<>();
-        int s=0;
-        for(int i=nums.length-1;i>0;i--){
-            ans.add(nums[i]);
-            s+=nums[i];
-            sum-=nums[i];
-            if(s>sum)break;
-        }
-        return ans;
-    }
+    int mod = 1000_000_007;
+    //===========  method ==================
 }
