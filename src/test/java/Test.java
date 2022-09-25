95fc4ade7149775d261854f176963dea1105b4df
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-09-25 23:36:03 +0800

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,15 @@
+import org.fusesource.jansi.Ansi;
+
+import static org.fusesource.jansi.Ansi.Color.GREEN;
+import static org.fusesource.jansi.Ansi.Color.RED;
+
 class Test {
+    int mod = 1000_000_007;
+
+    //===========  method ==================
     public static void main(String[] args) {
-
+        System.out.println("DPZ EXE");
+        System.out.println(Ansi.ansi().fg(RED).a("Hello").fg(GREEN).a(" World").reset().a(" DPZ"));
     }
-
 }
+
