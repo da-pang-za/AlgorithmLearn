ee93acf948e7d9050cbc50c18fc17a0bc169fc08
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-10-23 19:52:16 +0800

    fix

--- src/main/java/com/dpz/template/API.java
+++ src/main/java/com/dpz/template/API.java
@@ -79,7 +79,7 @@ public class API {
         return l;
     }
 
-    //第一个<=x的元素
+    //最后一个<=x的元素
     public Integer floor(int[] nums, int start, int end, int x) {
         if (x < nums[start]) return null;
         int l = start, r = end;
@@ -89,7 +89,6 @@ public class API {
             else r = mid - 1;
         }
         return l;
-
     }
 
     //第一个>=x的元素的idx(下标)
