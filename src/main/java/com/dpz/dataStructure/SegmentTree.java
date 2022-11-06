69734775c3a777efc9d157d936f9a71554a0926f
Big Fatty <54436289+da-pang-za@users.noreply.github.com>
2022-11-06 22:00:12 +0800

    线段树上二分

--- src/main/java/com/dpz/dataStructure/SegmentTree.java
+++ src/main/java/com/dpz/dataStructure/SegmentTree.java
@@ -1,11 +1,12 @@
 package com.dpz.dataStructure;
-//线段树讲解：https://zhuanlan.zhihu.com/p/106118909
-//例题：https://leetcode-cn.com/problems/range-sum-query-mutable/
+
 /*
  * 线段树
  * 线段树的用处就是，对编号连续的一些点进行修改或者统计操作，修改和统计的复杂度都是O(log2(n)).
  * 线段树中，每个点代表一条线段（区间）
- * <p>
+ * 线段树讲解：https://zhuanlan.zhihu.com/p/106118909
+ * 例题：https://leetcode-cn.com/problems/range-sum-query-mutable/
+ *
  * 用线段树统计的东西，必须符合区间加法
  * 符合区间加法的例子：
  * 数字之和——总数字之和 = 左区间数字之和 + 右区间数字之和
@@ -13,11 +14,12 @@ package com.dpz.dataStructure;
  * 最大值——总最大值=max(左区间最大值，右区间最大值)
  * 不符合区间加法的例子：
  * 众数——只知道左右区间的众数，没法求总区间的众数
- * <p>
+ *
+ * 线段树上二分(其实很简单)：https://leetcode.cn/problems/booking-concert-tickets-in-groups/solution/by-tsreaper-mnr7/
  */
 
 /**
- * 数组实现
+ * 线段树数组实现
  * https://zhuanlan.zhihu.com/p/106118909
  * https://leetcode-cn.com/problems/range-sum-query-mutable/solution/meng-xin-de-di-yi-ke-xian-duan-shu-by-98-c3es/
  */
@@ -53,7 +55,7 @@ class SegmentTree {
     public SegmentTree(int[] _nums, int _n, A add, M merge, MN mergeN, int mode) {
         this.m = merge;
         this.mn = mergeN;
-        this.a=add;
+        this.a = add;
         this.mode = mode;
         n = MAXN = _n;
         nums = _nums;
@@ -147,36 +149,6 @@ class SegmentTree {
         lazy[p] = 0;
     }
 }
-
-//demo:690.掉落的方块
-//class Solution {
-//    public List<Integer> fallingSquares(int[][] positions) {
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//        int idx = 0;
-//        TreeSet<Integer> set = new TreeSet<>();
-//        for (int[] p : positions) {
-//            set.add(p[0]);
-//            set.add(p[1] + p[0] - 1);
-//        }
-//        for (Integer v : set) {
-//            map.put(v, ++idx);
-//        }
-//        int n = idx;
-//        SegmentTree segmentTree = new SegmentTree(null, n, (a, b) -> Math.max(a, b), (a, b) -> a, SegmentTree.ASSIGN);
-//        List<Integer> ans = new ArrayList<>();
-//        for (int[] p : positions) {
-//            int l = map.get(p[0]);
-//            int r = map.get(p[0] + p[1] - 1);
-//
-//            //当前范围最高的
-//            int h = (int) segmentTree.query(l, r);
-//            segmentTree.update(l, r, h + p[1]);
-//            ans.add((int) segmentTree.query(1, n));
-//        }
-//        return ans;
-//    }
-//}
-
 
 
 
